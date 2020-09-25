fe5d41ebea66375fa917c2faa6bd5ffcb8541e6d
Mr Viet <vietdat657@gmail.com>
2020-09-25 08:57:07 +0700

    Done toolbar

--- kogi-web/src/components/NavarBar/index.vue
+++ kogi-web/src/components/NavarBar/index.vue
@@ -1,9 +1,19 @@
 <template>
   <v-app id="inspire">
-    <v-app-bar :clipped-left="$vuetify.breakpoint.lgAndUp" app color="primary" dark>
-      <!--      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />-->
+    <v-app-bar
+      :clipped-left="$vuetify.breakpoint.lgAndUp"
+      app
+      color="primary"
+      dark
+    >
       <v-toolbar-title style="width: 350px">
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-app-bar-nav-icon
+          @click.stop="handleClick"
+          class="ma-2"
+          label="Mini variant"
+          
+        ></v-app-bar-nav-icon>
+
         <a href="/" class="white--text" style="text-decoration: none">
           &nbsp;Kogi Store
         </a>
@@ -20,78 +30,87 @@
       <v-btn icon>
         <v-icon>mdi-account-circle</v-icon>
       </v-btn>
-      <v-btn icon>
-        <v-badge content="2" value="2" color="green" overlap>
-          <v-icon>mdi-bell</v-icon>
-        </v-badge>
-      </v-btn>
-      <v-btn href="/cart" icon>
-        <v-badge content="2" value="2" color="green" overlap>
-          <v-icon>mdi-cart</v-icon>
-        </v-badge>
-      </v-btn>
     </v-app-bar>
 
-    <sideBar />
+    <sideBar :valueMiniVariant="miniVariant" v-on:drawerPropsChange="handleEmit" :drawerProps="drawer" />
     <v-main>
       <router-view />
-      
-    <v-footer :padless="true">
-      <v-card flat tile width="100%" class="secondary white--text text-center">
-        <v-card-text>
-          <v-btn class="mx-4 white--text" icon>
-            <v-icon size="24px">mdi-home</v-icon>
-          </v-btn>
-          <v-btn class="mx-4 white--text" icon>
-            <v-icon size="24px">mdi-email</v-icon>
-          </v-btn>
-          <v-btn class="mx-4 white--text" icon>
-            <v-icon size="24px">mdi-calendar</v-icon>
-          </v-btn>
-          <v-btn class="mx-4 white--text" icon>
-            <v-icon size="24px">mdi-delete</v-icon>
-          </v-btn>
-        </v-card-text>
 
-        <v-card-text
-          class="white--text pt-0"
-        >Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</v-card-text>
+      <v-footer :padless="true">
+        <v-card
+          flat
+          tile
+          width="100%"
+          class="secondary white--text text-center"
+        >
+          <v-card-text>
+            <v-btn class="mx-4 white--text" icon>
+              <v-icon size="24px">mdi-home</v-icon>
+            </v-btn>
+            <v-btn class="mx-4 white--text" icon>
+              <v-icon size="24px">mdi-email</v-icon>
+            </v-btn>
+            <v-btn class="mx-4 white--text" icon>
+              <v-icon size="24px">mdi-calendar</v-icon>
+            </v-btn>
+            <v-btn class="mx-4 white--text" icon>
+              <v-icon size="24px">mdi-delete</v-icon>
+            </v-btn>
+          </v-card-text>
 
-        <v-divider></v-divider>
+          <v-card-text class="white--text pt-0"
+            >Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet.
+            Mauris cursus commodo interdum. Praesent ut risus eget metus luctus
+            accumsan id ultrices nunc. Sed at orci sed massa consectetur
+            dignissim a sit amet dui. Duis commodo vitae velit et faucibus.
+            Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum
+            ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel
+            diam elementum tempor vel ut orci. Orci varius natoque penatibus et
+            magnis dis parturient montes, nascetur ridiculus mus.</v-card-text
+          >
 
-        <v-card-text class="white--text">
-          {{ new Date().getFullYear() }} —
-          <strong>ShipIT</strong>
-        </v-card-text>
-      </v-card>
-    </v-footer>
+          <v-divider></v-divider>
+
+          <v-card-text class="white--text">
+            {{ new Date().getFullYear() }} —
+            <strong>ShipIT</strong>
+          </v-card-text>
+        </v-card>
+      </v-footer>
     </v-main>
-
   </v-app>
 </template>
 <script>
 import sideBar from "../SideBar/index";
 
 export default {
+    name: "NavBar",
   data() {
     return {
-      items: [
-        { title: "T-Shirts" },
-        { title: "Jackets" },
-        { title: "Shirts" },
-        { title: "Jeans" },
-        { title: "Shoes" },
-      ],
-      activeBtn: 1,
+      drawer: false,
+      
+      miniVariant: false,
     };
   },
   components: {
     sideBar,
   },
+  
+  methods: {
+    handleClick() {
+      if (this.$vuetify.breakpoint.mdAndUp) {
+        this.miniVariant = !this.miniVariant;
+      }
+      else this.drawer = !this.drawer
+    },
+    handleEmit(value){
+      this.drawer=value;
+    }
+  },
 };
 </script>
 <style scoped>
-.v-navigation-drawer{
-    margin-top: 65px ;
+.v-navigation-drawer {
+  margin-top: 65px;
 }
 </style>
